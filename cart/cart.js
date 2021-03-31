07aca4934ab0c2295114d271ad60214734a661cb
Kalan Prudhomme <cleaver9@gmail.com>
2021-03-31 13:09:24 -0700

    created cart array

--- cart/cart.js
+++ cart/cart.js
@@ -1,53 +1,28 @@
-export const itemShop = [
+export const cartList = [
 
     { 
-        name: 'Deku Shield',
-        image: '../assets/deku shield.jpg',
-        description: 'Protect yourself from Octoroks!',
-        category: 'Weapon',
-        price: 40,
-        color: 'beige',
         id: '1',
+        quantity: 1,
     },
 
     { 
-        name: 'Arrows',
-        image: '../assets/arrows.png',
-        description: 'Can\'t use a bow without these!',
-        category: 'Ammo',
-        price: 20,
-        color: 'Pink',
         id: '2',
+        quantity: 4,
     },
 
     { 
-        name: 'Bombs',
-        image: '../assets/bomb.jpg',
-        description: 'Blast away rocks!',
-        category: 'Weapon',
-        price: 25,
-        color: 'lightblue',
         id: '3',
+        quantity: 2,
     },
 
     { 
-        name: 'Red Potion',
-        image: '../assets/red-potion.jpg',
-        description: 'Restores 10 Hearts.',
-        category: 'Ammo',
-        price: 30,
-        color: 'pink',
         id: '4',
+        quantity: 3,
     },
 
     { 
-        name: 'Boomerang',
-        image: '../assets/boomerang.jpg',
-        description: 'Useful for striking out of reach switches.',
-        category: 'Weapon',
-        price: 50,
-        color: 'Yellow',
         id: '5',
+        quantity: 1,
     },
 ];
 
